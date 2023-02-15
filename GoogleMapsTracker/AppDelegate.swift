8e7239684adf8cfa96ffd79cb687b2e2f3738f5f
OksanaKam <moremore1@yandex.ru>
2023-02-15 21:22:48 +0300

    Добавить CLLocationManager

--- GoogleMapsTracker/AppDelegate.swift
+++ GoogleMapsTracker/AppDelegate.swift
@@ -15,7 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        GMSServices.provideAPIKey("String")
         
         return true
     }
